8ad4b71d2dcbc7deb23f6b35c32530ea43879d1d
Angel <a4k0o@protonmail.com>
2022-11-08 13:57:50 +0200

    add spring project

---
+++ src/main/java/com/example/spingbootblogapplication/SpingBootBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingbootblogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spingbootblogapplication/SpingBootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingbootblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
