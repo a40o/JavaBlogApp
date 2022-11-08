135ba8539f3789c6314c1c40c7fd5eb521fe3f6b
Angel <a4k0o@protonmail.com>
2022-11-08 14:04:29 +0200

    edit properties & pom files

--- src/main/java/com/example/spingbootblogapplication/SpringBootBlogApplication.java
+++ src/main/java/com/example/spingbootblogapplication/SpringBootBlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpingBootBlogApplication {
+public class SpringBootBlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpingBootBlogApplication.class, args);
+		SpringApplication.run(SpringBootBlogApplication.class, args);
 	}
 
 }
