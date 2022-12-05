dcad9b3a9f509383b9ce10ba1727577f1709cc76
a4ko <a4k0o@protonmail.com>
2022-12-05 12:01:08 +0200

    new post

--- src/main/java/com/example/spingbootblogapplication/controllers/PostController.java
+++ src/main/java/com/example/spingbootblogapplication/controllers/PostController.java
@@ -1,17 +1,24 @@
 package com.example.spingbootblogapplication.controllers;
 
+import com.example.spingbootblogapplication.models.Account;
 import com.example.spingbootblogapplication.models.Post;
+import com.example.spingbootblogapplication.services.AccountService;
 import com.example.spingbootblogapplication.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
 public class PostController {
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
@@ -26,4 +33,24 @@ public class PostController {
             return "404";
         }
     }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(Model model) {
+        Optional<Account> optionalAccount = accountService.findByEmail("abv");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_new";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post) {
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
 }
