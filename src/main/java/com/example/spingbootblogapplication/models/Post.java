53a08798c0f863dc67c56331acc531788d6d17b3
a4ko <a4k0o@protonmail.com>
2022-12-12 15:59:02 +0200

    better data display & background

--- src/main/java/com/example/spingbootblogapplication/models/Post.java
+++ src/main/java/com/example/spingbootblogapplication/models/Post.java
@@ -4,6 +4,7 @@ import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.format.DateTimeFormatter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -32,6 +33,14 @@ public class Post {
     @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
     private Account account;
 
+
+    public String convertDate(LocalDateTime date){
+        return DateTimeFormatter.ISO_DATE.format(date);
+    }
+
+    public String convertTime(LocalDateTime date){
+        return DateTimeFormatter.ISO_LOCAL_TIME.format(date.withNano(0));
+    }
     @Override
     public String toString() {
         return "Post{" +
